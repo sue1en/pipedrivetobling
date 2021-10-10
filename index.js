434654b23633fa5edebc0876c42b9bd2fad9dc7b
Suelen dos Santos Batista <suelen.batista@al.infnet.edu.br>
2021-10-09 21:14:36 -0300

    conection to pipedrive

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const pipedrive = require("pipedrive");
+const cors = require("cors");
+const { name, version } = require("./package.json")
+const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+
+const apiKey = String(process.env.PIPEDRIVE_API_KEY)
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const defaultClient = pipedrive.ApiClient.instance;
+
+let apiToken = defaultClient.authentications.api_key;
+apiToken.apiKey = apiKey;
+
+app.get('/', async (req, res) => {
+    const api = new pipedrive.DealsApi();
+    const deals = await api.getDeals();
+
+    res.send(deals.data);
+});
+
+app.listen(
+  port,
+  () => console.log(
+      `The server is running at the port http://localhost:${port} \nProject Name: ${name}, Version: ${version}`
+    )
+);
